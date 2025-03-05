b23009b4d8a7f40aeccc4801f2d6c4a6f7e55f54
tim1021 <57831473+tim1021@users.noreply.github.com>
2025-03-05 20:04:18 +0800

    Add files via upload

---
+++ assiginment2/ConsoleApp3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int limit = 100;
+        List<int> primes = SieveOfEratosthenes(limit);
+
+        Console.WriteLine("2~" +100+ " 以内的素数为：");
+        foreach (int prime in primes)
+        {
+            Console.Write(prime + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static List<int> SieveOfEratosthenes(int limit)
+    {
+        bool[] isPrime = new bool[limit + 1];
+        for (int i = 2; i <= limit; i++)
+        {
+            isPrime[i] = true;
+        }
+
+        for (int p = 2; p * p <= limit; p++)
+        {
+            if (isPrime[p])
+            {
+                for (int i = p * p; i <= limit; i += p)
+                {
+                    isPrime[i] = false;
+                }
+            }
+        }
+
+        List<int> primes = new List<int>();
+        for (int i = 2; i <= limit; i++)
+        {
+            if (isPrime[i])
+            {
+                primes.Add(i);
+            }
+        }
+
+        return primes;
+    }
+}
---
+++ assiginment2/ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Program
+{
+    public bool IsToeplitzMatrix(int[][] matrix)
+    {
+        int m = matrix.Length;
+        int n = matrix[0].Length;
+
+        for (int i = 0; i < m - 1; i++)
+        {
+            for (int j = 0; j < n - 1; j++)
+            {
+                if (matrix[i][j] != matrix[i + 1][j + 1])
+                {
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+
+    public static void Main(string[] args)
+    {
+        Program p = new Program();
+
+        int[][] matrix1 = new int[][] {
+            new int[] { 1, 2, 3, 4 },
+            new int[] { 5, 1, 2, 3 },
+            new int[] { 9, 5, 1, 2 }
+        };
+
+        int[][] matrix2 = new int[][] {
+            new int[] { 1, 2 },
+            new int[] { 2, 2 }
+        };
+
+        Console.WriteLine(p.IsToeplitzMatrix(matrix1));  // t
+        Console.WriteLine(p.IsToeplitzMatrix(matrix2));  // f
+    }
+}
