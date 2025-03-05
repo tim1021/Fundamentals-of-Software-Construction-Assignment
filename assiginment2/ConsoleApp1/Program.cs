57f72becc75e30b9cbda6d3cf5b352aafb5ae6d3
tim1021 <57831473+tim1021@users.noreply.github.com>
2025-03-05 20:02:28 +0800

    Add files via upload

---
+++ assiginment2/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿// 输出质因数
+using System;
+using System.Security.Cryptography.X509Certificates;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static bool IsPrime(int num)
+        {
+            if (num <= 1)
+                return false;
+            if (num == 2)
+                return true;
+            if (num % 2 == 0)
+                return false;
+            for (int i=3;i * i <= num ; i += 2)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static void FindPrimeFactors(int num)
+        {
+            Console.WriteLine($"prime factors of {num} are:");
+
+            for (int i = 2;i <= num ;i++)
+            {
+                while (num % i == 0 && IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                    num /= i;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number for print prime factor:");
+            int number = int.Parse(Console.ReadLine());
+            FindPrimeFactors(number);
+        }
+    }
+}
