d633b7795cad30d0306df812d8d29d94a6fec7c6
tim1021 <57831473+tim1021@users.noreply.github.com>
2025-03-19 22:35:01 +0800

    Add files via upload

---
+++ assignment4/ConsoleApp1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+}
+
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+    public GenericList()
+    {
+        tail = head = null;
+    }
+    public Node<T> Head
+    {
+        get => head;
+    }
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+        if (tail == null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+    public void ForEach(Action<T> action)
+    {
+        for (Node<T> node = head; node != null; node = node.Next)
+        {
+            action(node.Data);
+        }
+    }
+    public T GetMax()
+    {
+        if (head == null)
+            throw new InvalidOperationException("List is empty.");
+
+        T max = head.Data;
+        ForEach(item =>
+        {
+            if (Comparer<T>.Default.Compare(item, max) > 0)
+            {
+                max = item;
+            }
+        });
+        return max;
+    }
+    public T GetMin()
+    {
+        if (head == null)
+            throw new InvalidOperationException("List is empty.");
+
+        T min = head.Data;
+        ForEach(item =>
+        {
+            if (Comparer<T>.Default.Compare(item, min) < 0)
+            {
+                min = item;
+            }
+        });
+        return min;
+    }
+    public T GetSum()
+    {
+        dynamic sum = 0;
+        ForEach(item =>
+        {
+            sum += item;
+        });
+        return sum;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // 整型list
+        GenericList<int> intlist = new GenericList<int>();
+        for (int x = 0; x < 10; x++)
+        {
+            intlist.Add(x);
+        }
+
+        // 打印元素
+        Console.WriteLine("intlist elements:");
+        intlist.ForEach(item => Console.WriteLine(item));
+
+        // 求最大值、最小值、求和
+        Console.WriteLine($"Max: {intlist.GetMax()}");
+        Console.WriteLine($"Min: {intlist.GetMin()}");
+        Console.WriteLine($"Sum: {intlist.GetSum()}");
+
+        // 字符串型list
+        GenericList<string> stringlist = new GenericList<string>();
+        for (int x = 0; x < 10; x++)
+        {
+            stringlist.Add("str" + x);
+        }
+
+        // 打印字符串元素
+        Console.WriteLine("stringlist elements:");
+        stringlist.ForEach(item => Console.WriteLine(item));
+    }
+}
---
+++ assignment4/ConsoleApp2/Program.cs
@@ -0,0 +1,87 @@
+﻿//使用事件机制，模拟实现一个闹钟功能。
+//闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+//在闹钟走时时或者响铃时，在控制台显示提示信息。
+
+using System;
+
+class AlarmClock
+{
+    private DateTime alarmTime;
+    private Timer timer;
+    public event Action Tick;
+    public event Action Alarm;
+
+    public AlarmClock()
+    {
+        timer = new Timer(OnTick, null, Timeout.Infinite, 1000);  // 每秒触发一次
+    }
+
+    public void SetAlarmTime(int hour, int minute,int second)
+    {
+        alarmTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, hour, minute, second);
+    }
+
+    public void Start()
+    {
+        if (alarmTime == default(DateTime))
+        {
+            Console.WriteLine("set time first");
+            return;
+        }
+
+        Console.WriteLine("clock starts");
+        timer.Change(0, 1000);
+    }
+
+    public void Stop()
+    {
+        timer.Change(Timeout.Infinite, Timeout.Infinite);  // 停止定时器
+        Console.WriteLine("stop...");
+    }
+
+    private void OnTick(object state)
+    {
+        Tick?.Invoke();  
+
+        CheckAlarm();
+    }
+    private void CheckAlarm()
+    {
+        if (DateTime.Now.Hour == alarmTime.Hour && 
+            DateTime.Now.Minute == alarmTime.Minute&& 
+            DateTime.Now.Second == alarmTime.Second)
+        {
+            Alarm?.Invoke(); 
+            Stop();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        AlarmClock alarmClock = new AlarmClock();
+
+        // 监听tick事件
+        alarmClock.Tick += () =>
+        {
+            Console.WriteLine("tick");
+        };
+
+        // 监听alarm事件
+        alarmClock.Alarm += () =>
+        {
+            Console.WriteLine("alarm");
+        };
+
+        DateTime currentTime = DateTime.Now;
+        // 设置alarmtime
+        alarmClock.SetAlarmTime(currentTime.Hour, currentTime.Minute,currentTime.Second+10);
+    
+        alarmClock.Start();
+        Console.WriteLine("press anykey to stop");
+        Console.ReadKey();
+        alarmClock.Stop();
+    }
+}
